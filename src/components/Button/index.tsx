a355d009b1e5ff0d8b0e3ce18d10aba80ae1662c
Evandro Calado <evandrocalado07@gmail.com>
2024-01-02 21:07:09 -0300

    ✨ feat(pages): login page

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,31 @@
 import { ButtonHTMLAttributes, FC } from 'react';
+import { useNavigation } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const Button: FC<ButtonProps> = ({ children, ...rest }) => {
+export const Button: FC<ButtonProps> = ({ children, className, ...rest }) => {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
-    <button className="btn btn-primary" {...rest}>
-      {children}
+    <button
+      className={twMerge('btn btn-primary btn-block', className)}
+      disabled={isSubmitting}
+      {...rest}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span>
+          sending...
+        </>
+      ) : (
+        children
+      )}
     </button>
   );
 };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,47 @@
+import { Form, Link } from 'react-router-dom';
+import { Button, Input } from '../../components';
+
 export const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <section className="grid h-screen place-items-center">
+      <Form
+        method="post"
+        className="card flex w-96 flex-col gap-y-4 bg-base-100 p-8 shadow-lg"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+
+        <Input
+          label="Email"
+          type="email"
+          name="identifier"
+          defaultValue="test@gmail.com"
+        />
+
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          defaultValue="secret"
+        />
+
+        <Button type="submit" className="mt-4">
+          Login
+        </Button>
+
+        <Button type="button" className="btn btn-secondary">
+          Guest User
+        </Button>
+
+        <p className="text-center">
+          Not a member yet?{' '}
+          <Link
+            to="/register"
+            className="link-hover link link-primary ml-2 capitalize"
+          >
+            Register
+          </Link>
+        </p>
+      </Form>
+    </section>
   );
 };
