7b288e7a256007b4f1f1b0eae3d23d3ba0e49442
Evandro Calado <evandrocalado07@gmail.com>
2024-01-04 22:01:59 -0300

    ✨ feat(components): featured products

---
+++ src/components/FeaturedProducts/index.tsx
@@ -0,0 +1,10 @@
+import { ProductsGrid, SectionTitle } from '..';
+
+export const FeaturedProducts = () => {
+  return (
+    <div className="pt-24">
+      <SectionTitle>Featured Products</SectionTitle>
+      <ProductsGrid />
+    </div>
+  );
+};
