3dfeefc76db08ce57605b41b6f3655b26f78f560
Evandro Calado <evandrocalado07@gmail.com>
2024-01-06 18:47:04 -0300

    ✨ feat(components): loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <span className="loading loading-ring loading-lg text-primary"></span>
+    </div>
+  );
+};
