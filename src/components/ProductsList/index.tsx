6092a568cfcadb1070b9c64b4a4daaa5b05ef3c4
Evandro Calado <evandrocalado07@gmail.com>
2024-01-06 00:16:53 -0300

    ✨ feat(components): products list

---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,46 @@
+import { Link, useLoaderData } from 'react-router-dom';
+import { TypeFeatured } from '../../types/type-featured';
+
+export const ProductsList = () => {
+  const products = useLoaderData() as TypeFeatured;
+
+  return (
+    <div className="mt-12 grid gap-y-8">
+      {products.data.map((product) => {
+        const { image, title, price, category, featured } = product.attributes;
+
+        return (
+          <Link
+            key={product.id}
+            to={`/products/${product.id}`}
+            className="group flex flex-col flex-wrap gap-y-4 overflow-hidden rounded-2xl bg-base-100 shadow-lg transition duration-300 hover:shadow-xl sm:flex-row"
+          >
+            <figure className="sm:w-1/3">
+              <img
+                src={image.data.attributes.formats.small.url}
+                alt={title}
+                className="h-64 w-full object-cover transition duration-300 group-hover:scale-105 md:h-48"
+              />
+            </figure>
+
+            <div className="card-body">
+              <div className="flex items-center gap-2">
+                <h2 className="card-title capitalize tracking-wider">
+                  {title}
+                </h2>
+                {featured && (
+                  <span className="badge badge-primary uppercase">new</span>
+                )}
+              </div>
+
+              <div className="card-actions mt-4 items-center justify-between">
+                <span className="text-secondary">R${price}</span>
+                <div className="badge badge-outline">{category}</div>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
