e1c92c5db58932a7d0d64c68bdd1e92f8e463ca1
Evandro Calado <evandrocalado07@gmail.com>
2024-01-07 21:22:39 -0300

    🦄 refactor(api): return a object

--- src/api/featuredLoader.ts
+++ src/api/featuredLoader.ts
@@ -4,7 +4,7 @@ import { customFetch } from '../utils';
 
 export const featuredLoader = async ({ params }: { params: Params }) => {
   const response = await customFetch(`/products/${params.id}?populate=*`);
-  const product = response.data as TypeProduct;
+  const product: TypeProduct = response.data as TypeProduct;
 
-  return product;
+  return { product };
 };
--- src/api/landingLoader.ts
+++ src/api/landingLoader.ts
@@ -1,10 +1,10 @@
-import { TypeFeatured } from '../types/type-featured';
+import { TypeProducts } from '../types/type-products';
 import { customFetch } from '../utils';
 
 const url = '/products?populate=*&filters[featured][$eq]=true';
 
 export const landingLoader = async () => {
   const response = await customFetch(url);
-  const produtcs: TypeFeatured = response.data;
-  return produtcs;
+  const products = response.data as TypeProducts;
+  return { products };
 };
