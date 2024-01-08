9b96b7f02cbf0978f3c4e24d2c2e6b80c72455f6
Evandro Calado <evandrocalado07@gmail.com>
2024-01-07 21:26:04 -0300

    ✨ feat(pages): add loading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import { Header } from '../../components';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Header, Loading, Navbar } from '../../components';
 
 export const Home = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+
   return (
     <>
       <Header />
+      <Navbar />
 
-      <section className="align-element py-20">
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
