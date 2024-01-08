bcf9397dfdced45ad4d37e9f242d124c23268822
Evandro Calado <evandrocalado07@gmail.com>
2024-01-08 19:55:47 -0300

    ✨ feat(store): clear/remove/edit
    clear, remove and edit methods on reducers

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -43,24 +43,50 @@ const cartSlice = createSlice({
       const { product } = action.payload as {
         product: ProductProps;
       };
-
       const item = state.cartItems.find((i) => i.cartID === product.cartID);
       if (item) {
         item.amount += product.amount;
       } else {
         state.cartItems.push(product);
       }
-
       state.numItemsInCart += product.amount;
       state.cartTotal += product.amount * product.price;
-
       cartSlice.caseReducers.calculateTotals(state);
-
       toast.success('Item added to cart');
     },
-    clearCart: (state) => {},
-    removeItem: (state, action) => {},
-    editItem: (state, action) => {},
+
+    clearCart: () => {
+      localStorage.setItem('cart', JSON.stringify(initialState));
+      return initialState;
+    },
+
+    removeItem: (state, action) => {
+      const { cartID } = action.payload as { cartID: string };
+      const product: ProductProps = state.cartItems.find(
+        (i) => i.cartID === cartID,
+      )!;
+      state.cartItems = state.cartItems.filter((i) => i.cartID !== cartID);
+      state.numItemsInCart -= product.amount;
+      state.cartTotal -= product.amount * product.price;
+      toast.error('Item removed from cart');
+    },
+
+    editItem: (state, action) => {
+      const { cartID, amount } = action.payload as {
+        cartID: string;
+        amount: number;
+      };
+      const item = state.cartItems.find((i) => i.cartID === cartID);
+
+      if (item) {
+        state.numItemsInCart += amount - item.amount;
+        state.cartTotal += item.price * (amount - item.amount);
+        item.amount = amount;
+        cartSlice.caseReducers.calculateTotals(state);
+        toast.success('Cart updated');
+      }
+    },
+
     calculateTotals: (state) => {
       state.tax = 0.1 * state.cartTotal;
       state.orderTotal = state.cartTotal + state.shipping + state.tax;
