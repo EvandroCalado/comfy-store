e5fe467ab6231c9e6b3f85dfce26d94d16100f6a
Evandro Calado <evandrocalado07@gmail.com>
2024-01-08 21:08:31 -0300

    ✨ feat(pages): cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,44 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { CartItemsList, CartTotals, SectionTitle } from '../../components';
+import { RootState } from '../../store';
+
 export const Cart = () => {
+  const user = null;
+  const numItemsInCart = useSelector(
+    (state: RootState) => state.cartState.numItemsInCart,
+  );
+
+  if (numItemsInCart === 0) {
+    return <SectionTitle>Your cart is empty</SectionTitle>;
+  }
+
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <>
+      <SectionTitle>Shopping Cart</SectionTitle>
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals />
+          {user ? (
+            <Link
+              to="/checkout"
+              className="btn btn-primary btn-block mt-8 uppercase"
+            >
+              proceed to checkout
+            </Link>
+          ) : (
+            <Link
+              to="/login"
+              className="btn btn-primary btn-block mt-8 uppercase"
+            >
+              login to checkout
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
