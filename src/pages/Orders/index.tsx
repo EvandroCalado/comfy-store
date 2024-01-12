fe5983923436b5c5373ceb0e835148f3437475c2
Evandro Calado <evandrocalado07@gmail.com>
2024-01-12 20:36:54 -0300

    ✨ feat(pages): create order ṕage

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,7 +1,19 @@
+import { useLoaderData } from 'react-router-dom';
+import { OrderList, PaginationContainer, SectionTitle } from '../../components';
+import { TypeOrder } from '../../types/type-order';
+
 export const Orders = () => {
+  const { meta } = useLoaderData() as { meta: TypeOrder['meta'] };
+
+  if (meta.pagination.total < 1) {
+    return <SectionTitle>Please make an order</SectionTitle>;
+  }
+
   return (
-    <div>
-      <h1>Orders</h1>
-    </div>
+    <>
+      <SectionTitle>You Orders</SectionTitle>
+      <OrderList />
+      <PaginationContainer />
+    </>
   );
 };
