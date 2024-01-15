8f43e1f6648592384668bb8a558d2d286321e5e5
Evandro Calado <evandrocalado07@gmail.com>
2024-01-15 20:28:27 -0300

    🌈 style(tailwind): remove unused themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ export default {
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['light', 'dark', 'dracula', 'emerald', 'valentine', 'nord'],
+    themes: ['light', 'dark'],
   },
 };
