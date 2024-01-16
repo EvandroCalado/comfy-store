6c809428608e7a64ddad8e96cee31bb89b03f42a
Evandro Calado <evandrocalado07@gmail.com>
2024-01-15 21:05:26 -0300

    🧪 test(test): remove tests

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,9 @@
 export const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div
+      className="flex h-screen items-center justify-center"
+      data-testid="loading"
+    >
       <span className="loading loading-ring loading-lg text-primary"></span>
     </div>
   );
