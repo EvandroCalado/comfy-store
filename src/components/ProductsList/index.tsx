03a43714d420b37c28e6be05881a0ccdd018b548
Evandro Calado <evandrocalado07@gmail.com>
2024-01-17 20:50:25 -0300

    🎈 perf(components): image lazy load

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { TypeFeatured } from '../../types/type-featured';
 
 export const ProductsList = () => {
+  const [imageLoading, setImageLoading] = useState(true);
   const { products }: { products: TypeFeatured } = useLoaderData() as {
     products: TypeFeatured;
   };
+
+  useEffect(() => {
+    if (products.data.length > 0) {
+      setImageLoading(false);
+    }
+  });
 
   return (
     <div className="mt-12 grid gap-y-8">
@@ -21,7 +29,11 @@ export const ProductsList = () => {
               <img
                 src={image.data.attributes.formats.small.url}
                 alt={title}
-                className="h-64 w-full object-cover transition duration-300 group-hover:scale-105 md:h-48"
+                className={`h-64 w-full object-cover transition duration-300 group-hover:scale-105 md:h-48 ${
+                  imageLoading
+                    ? 'scale-110 blur-3xl grayscale'
+                    : 'scale-100 blur-0 grayscale-0'
+                }`}
               />
             </figure>
 
